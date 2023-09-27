d788a355846b0de9ab74ec73a8b80b0b98f3d752
Florent TORNICELLI <florent@geode.com>
2023-09-27 12:03:53 +0200

    feat: Zappa subproject for remote configs

--- deploy.bash
+++ deploy.bash
@@ -6,16 +6,17 @@ fi
 
 if [ $BRANCH_NAME = "master" ]; then
     AWS_REGION="us-east-1"
-    STACK_NAME="analytics-prod"
+    ENVIRONMENT="prod"
 elif [ $BRANCH_NAME = "dev" ]; then
     AWS_REGION="eu-west-3"
-    STACK_NAME="analytics-dev"
+    ENVIRONMENT="dev"
 else
     AWS_REGION="eu-west-3"
-    STACK_NAME="analytics-branch"
+    ENVIRONMENT="branch"
 fi
 
 DIST_OUTPUT_BUCKET="analytics-output-bucket"
+STACK_NAME="analytics-$ENVIRONMENT"
 VERSION="v2"
 
 # Run following commands only the first time to create bucket.
@@ -23,15 +24,19 @@ VERSION="v2"
 
 cd ./deployment
 chmod +x ./build-s3-dist.sh
+chmod +x ./deploy-remote-config.sh
 
 # Build project
-./build-s3-dist.sh $DIST_OUTPUT_BUCKET $STACK_NAME $VERSION
+# ./build-s3-dist.sh $DIST_OUTPUT_BUCKET $STACK_NAME $VERSION
 
-# Store Regional Assets to S3
-aws s3 cp ./regional-s3-assets s3://$DIST_OUTPUT_BUCKET-$AWS_REGION/$STACK_NAME/$VERSION --recursive --acl bucket-owner-full-control
+# # Store Regional Assets to S3
+# aws s3 cp ./regional-s3-assets s3://$DIST_OUTPUT_BUCKET-$AWS_REGION/$STACK_NAME/$VERSION --recursive --acl bucket-owner-full-control
 
-# Store Global Assets to S3
-aws s3 cp ./global-s3-assets s3://$DIST_OUTPUT_BUCKET-$AWS_REGION/$STACK_NAME/$VERSION --recursive --acl bucket-owner-full-control
+# # Store Global Assets to S3
+# aws s3 cp ./global-s3-assets s3://$DIST_OUTPUT_BUCKET-$AWS_REGION/$STACK_NAME/$VERSION --recursive --acl bucket-owner-full-control
 
-# Deploy CloudFormation by creating/updating Stack
-aws cloudformation deploy --template-file ./global-s3-assets/game-analytics-pipeline.template --stack-name $STACK_NAME --capabilities CAPABILITY_IAM  --s3-bucket $DIST_OUTPUT_BUCKET-$AWS_REGION
+# Deploy Remote Config API Gateway
+./deploy-remote-config.sh $ENVIRONMENT $AWS_REGION
+
+# # Deploy CloudFormation by creating/updating Stack
+# aws cloudformation deploy --template-file ./global-s3-assets/game-analytics-pipeline.template --stack-name $STACK_NAME --capabilities CAPABILITY_IAM  --s3-bucket $DIST_OUTPUT_BUCKET-$AWS_REGION
---
+++ source/services/remote-config/utils/constants.py
@@ -0,0 +1,10 @@
+"""
+This module contains constants.
+"""
+MAX_ACTIVATED_ABTESTS = 4
+
+TABLE_ARTIFACTS_REPOSITORY = "geode_public_artifacts_repository"
+TABLE_ABTESTS = "abtests"
+TABLE_ABTESTS_HISTORY = "abtests_history"
+TABLE_REMOTE_CONFIGS = "remote_configs"
+TABLE_USERS_ABTESTS = "users_abtests"
